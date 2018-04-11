eb7573f800a010cbb7e79b8d2aeb33b70b40e149
Eli Polonsky <Eli.polonsky@gmail.com>
2018-04-11 18:36:19 +0300

    Default spec file location (#16)

--- ci.sh
+++ ci.sh
@@ -57,6 +57,6 @@ echo "Running tests"
 py.test --cov-report term-missing --cov=${program} ${program}/tests
 
 echo "Running release"
-${program} --repo iliapolo/${program} releaser release --sha release --binary-entrypoint pyci.spec
+${program} --repo iliapolo/${program} releaser release --sha release
 
 echo "Done!"
