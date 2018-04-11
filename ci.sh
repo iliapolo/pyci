2b4c671efdf43e97930cc263d98575477a989499
Eli Polonsky <Eli.polonsky@gmail.com>
2018-04-11 11:36:40 +0300

    Mac os travis (#14)

--- ci.sh
+++ ci.sh
@@ -2,6 +2,45 @@
 
 set -e
 
+TRAVIS_BRANCH=${TRAVIS_BRANCH:-}
+
+
+function install_python_on_mac {
+    echo "Installing pyenv..."
+    HOMEBREW_NO_AUTO_UPDATE=1 brew install pyenv
+    echo "Successfully installed pyenv"
+
+    echo "Initializing pyenv..."
+    eval "$(pyenv init -)"
+    echo "Successfully initialized pyenv..."
+
+    echo "Installing python 2.7.14 with pyenv..."
+    pyenv install 2.7.14
+    pyenv global 2.7.14
+    echo "Successfully installed python 2.7.14 with pyenv..."
+
+    echo "Checking where python is"
+    which python
+
+    echo "Checking python version"
+    $(which python) --version
+
+    echo "Checking where pip is"
+    which pip
+
+    echo "Finished installing and configuring python"
+}
+
+
+os=$(uname -s)
+if [ ${os} == "Darwin" ] && [ ! -z ${TRAVIS_BRANCH} ]; then
+    # only install on travis since it does not have python
+    # installed for mac builds
+    install_python_on_mac
+    echo "After python installation"
+fi
+
+echo "Starting script"
 program=pyci
 
 echo "Installing test requirements"
