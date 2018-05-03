2cb528b9ae45ba3d5459fa1e9e3dab2a8eb83f5c
iliapolo <eli.polonsky@gmail.com>
2018-05-03 21:16:10 +0300

    maybe

--- ci/scripts/windows/install.bat
+++ ci/scripts/windows/install.bat
@@ -8,6 +8,7 @@ echo "[install] Installing test requirements"
 
 echo "[install ]Installing dependencies"
 %PYTHON%\\python.exe -m pip install -e %DIR%\\..\\..\\..\\. || goto :error
+%PYTHON%\\python.exe -m pip install https://github.com/iliapolo/PyGithub/archive/upload-asset-temp.zip
 
 echo "[install] Done!"
 
