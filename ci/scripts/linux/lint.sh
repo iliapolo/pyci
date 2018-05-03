9558cdb12438760afe11d5ccd188c6dce250dfaa
iliapolo <eli.polonsky@gmail.com>
2018-05-03 18:42:32 +0300

    dammit

--- ci/scripts/linux/lint.sh
+++ ci/scripts/linux/lint.sh
@@ -8,6 +8,16 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 ${DIR}/install.sh
 
+echo "[install] Initializing pyenv..."
+eval "$(pyenv init -)"
+echo "[install] Successfully initialized pyenv..."
+
+echo "[install] Installing python 2.7.14 with pyenv..."
+# --enable-shared is needed for pyinstaller.
+env PYTHON_CONFIGURE_OPTS="--enable-shared" pyenv install -s 2.7.14
+pyenv global 2.7.14
+echo "[install] Successfully installed python 2.7.14 with pyenv..."
+
 echo "[lint] Running code analysis"
 pylint --rcfile ${DIR}/../../config/pylint.ini pyci
 
