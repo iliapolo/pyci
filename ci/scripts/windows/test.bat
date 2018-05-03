d4bb88288d85893f33eab26a6e03668e1210167f
iliapolo <eli.polonsky@gmail.com>
2018-05-03 21:18:39 +0300

    typo

--- ci/scripts/windows/test.bat
+++ ci/scripts/windows/test.bat
@@ -6,7 +6,7 @@ echo "[test] Starting script"
 CALL %DIR%\\install.bat
 
 echo "[test] Running tests"
-%PYTHON%\\Scripts\\py.test.exe -c %DIR%\\..\\..\\config\\pytest.init --cov-config=%DIR%\\..\\..\\config\\.coveragerc --cov=pyci pyci/tests || goto :error
+%PYTHON%\\Scripts\\py.test.exe -c %DIR%\\..\\..\\config\\pytest.ini --cov-config=%DIR%\\..\\..\\config\\.coveragerc --cov=pyci pyci/tests || goto :error
 
 echo "[test] Done!"
 
