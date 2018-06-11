1fb538056032acd0962cebe7bb83cf50bee60bb3
Eli Polonsky <Eli.polonsky@gmail.com>
2018-06-11 14:16:05 +0300

    Bump version to 0.4.0

--- setup.py
+++ setup.py
@@ -30,7 +30,7 @@ PROJECT_NAME = 'py-ci'
 
 setup(
     name=PROJECT_NAME,
-    version='0.3.0',
+    version='0.4.0',
     author='Eli Polonsky',
     author_email='eli.polonsky@gmail.com',
     packages=[
