725de7cedc97c8033052cd18570c50f147eafd2d
eli.polonsky <eli.polonsky@appsflyer.com>
2019-06-15 02:44:14 +0300

    Pass DOCKER_ env variables in tox

--- pyci/tests/distros.py
+++ pyci/tests/distros.py
@@ -194,7 +194,7 @@ def docker():
     path = utils.which('docker')
     if not path:
         raise RuntimeError('docker command not found')
-    return 'export DOCKER_HOST={} && docker'.format(os.environ.get('DOCKER_HOST'))
+    return path
 
 
 os_mapping = {
