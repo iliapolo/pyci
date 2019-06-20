d7f335b4b04a82c67857687a0526a38b98d98165
eli.polonsky <eli.polonsky@appsflyer.com>
2019-06-20 12:21:23 +0300

    Dont run pypi tests on binary package

--- pyci/tests/shell/subcommands/test_pypi.py
+++ pyci/tests/shell/subcommands/test_pypi.py
@@ -27,69 +27,35 @@ except ImportError:
 
 from twine.commands import upload
 
-import pytest
 from pyci.shell import secrets
 
 
-@pytest.mark.parametrize("binary", [False, True])
-def test_upload(pypi, binary, pyci, mocker):
+def test_upload(pypi, pyci, mocker):
 
-    if not binary:
-        mocker.patch(target='twine.commands.upload.main', new=MagicMock())
+    mocker.patch(target='twine.commands.upload.main', new=MagicMock())
 
-    expected_url = 'https://test.pypi.org/manage/project/py-ci/release/{}/'.format(
-        pyci.wheel_path.split('-')[1])
+    pypi.run('upload --wheel {}'.format(pyci.wheel_path))
 
-    result = pypi.run('upload --wheel {}'.format(pyci.wheel_path), binary=binary)
+    expected_args = ['--username', secrets.twine_username(),
+                     '--password', secrets.twine_password(),
+                     '--repository-url', 'https://test.pypi.org/legacy/',
+                     pyci.wheel_path]
 
-    if binary:
-
-        expected_output = 'Wheel uploaded: {}'.format(expected_url)
-
-        assert expected_output in result.std_out
-
-    else:
-
-        expected_args = ['--username', secrets.twine_username(),
-                         '--password', secrets.twine_password(),
-                         '--repository-url', 'https://test.pypi.org/legacy/',
-                         pyci.wheel_path]
-
-        # noinspection PyUnresolvedReferences
-        upload.main.assert_called_once_with(expected_args)  # pylint: disable=no-member
+    # noinspection PyUnresolvedReferences
+    upload.main.assert_called_once_with(expected_args)  # pylint: disable=no-member
 
 
-@pytest.mark.parametrize("binary", [False, True])
-def test_upload_already_published(pypi, pack, binary, mocker):
+def test_upload_already_published(pypi, pyci, mocker):
 
     # Mocking the response from PyPI in this case
     def _upload(*_, **__):
         raise BaseException('File already exists')
 
-    wheel_path = pack.api.wheel()
+    mocker.patch(target='twine.commands.upload.main', side_effect=_upload)
 
-    if not binary:
-        mocker.patch(target='twine.commands.upload.main', side_effect=_upload)
-
-    result = pypi.run('upload --wheel {}'.format(wheel_path), catch_exceptions=True, binary=binary)
+    result = pypi.run('upload --wheel {}'.format(pyci.wheel_path), catch_exceptions=True)
 
     expected_output = 'A wheel with the same name as {} was already uploaded' \
-        .format(os.path.basename(wheel_path))
+        .format(os.path.basename(pyci.wheel_path))
 
-    if binary:
-
-        # change the source code but create a wheel with the same version
-        main = os.path.join(pack.api.repo_dir, 'pyci', 'shell', 'main.py')
-        with open(main, 'w') as stream:
-            stream.write('import os')
-
-        os.remove(wheel_path)
-        wheel_path = pack.api.wheel()
-
-        result = pypi.run('upload --wheel {}'.format(wheel_path), catch_exceptions=True, binary=binary)
-
-        assert expected_output in result.std_out
-
-    else:
-
-        assert expected_output in result.std_out
+    assert expected_output in result.std_out
