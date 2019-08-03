2f88e9e2b88e4297f205522f7318888a5175c1a1
Eli Polonsky <Eli.polonsky@gmail.com>
2019-08-03 16:22:28 +0300

    Bump version to 0.8.0

--- setup.py
+++ setup.py
@@ -29,7 +29,7 @@ PROJECT_NAME = 'py-ci'
 setup(
     name='py-ci',
     url='https://github.com/iliapolo/pyci/',
-    version='0.7.3',
+    version='0.8.0',
     author='Eli Polonsky',
     author_email='eli.polonsky@gmail.com',
     packages=[
